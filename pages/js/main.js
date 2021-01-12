1722c8d94c449e5d8142b8f259d95e988b2ccff0
Aleksey Vokhmin <vohmina2011@yandex.ru>
2021-01-12 21:05:03 +0300

    New site
    New index page with assets

---
+++ pages/js/main.js
@@ -0,0 +1,11 @@
+/*$(document).ready(function() {
+    var stickyOffset = $('.header').offset().top;
+
+    $(window).scroll(function(){
+    var sticky = $('.header'),
+        scroll = $(window).scrollTop();
+
+    if (scroll >= stickyOffset) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+    });
+})*/
