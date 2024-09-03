804792515c69f1d57a8000c7a3b968904376bc96
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-09-04 00:29:16 +0300

    Add social links, and improve project styling

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "./components/ui/button";
-import { ExternalLink } from "lucide-react";
+import { ExternalLink, MailIcon } from "lucide-react";
+import {
+  SiVk,
+  SiTelegram,
+  SiGithub,
+  SiDiscord,
+  type IconType,
+} from "@icons-pack/react-simple-icons";
 import BioconLogo from "./assets/biocon-logo.png";
 import ChemLabLogo from "./assets/chem-lab-logo.jpg";
 import LmsLogo from "./assets/lms-logo.png";
@@ -30,7 +37,7 @@ function ProjectCard({
   return (
     <Card className="flex flex-col">
       <CardHeader>
-        <CardTitle className="inline-flex items-center gap-2">
+        <CardTitle className="inline-flex items-center gap-4">
           {title}
           {image && (
             <Avatar>
@@ -52,7 +59,7 @@ function ProjectCard({
       <CardContent>
         <p className="text-base md:text-lg">{description}</p>
       </CardContent>
-      <CardFooter className="flex flex-row gap-2 mt-auto">
+      <CardFooter className="flex flex-row gap-4 mt-auto">
         {url && (
           <Button variant="outline" size="icon" asChild>
             <a href={url} target="_blank">
@@ -63,20 +70,22 @@ function ProjectCard({
         {github && (
           <Button variant="outline" size="icon" asChild>
             <a href={github} target="_blank">
-              <svg
-                role="img"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 invert"
-              >
-                <title>GitHub</title>
-                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-              </svg>
+              <SiGithub />
             </a>
           </Button>
         )}
       </CardFooter>
     </Card>
+  );
+}
+
+function SocialLink({ href, Icon }: { href: string; Icon: IconType }) {
+  return (
+    <Button variant="outline" size="icon-auto" asChild>
+      <a href={href} target="_blank">
+        <Icon size={32} />
+      </a>
+    </Button>
   );
 }
 
@@ -124,6 +133,19 @@ function App() {
             </a>
             .
           </p>
+        </section>
+        <section>
+          <h2>Контакты</h2>
+          <div className="flex flex-row gap-4 justify-center">
+            <SocialLink href="mailto:vohmina2011@yandex.ru" Icon={MailIcon} />
+            <SocialLink href="https://vk.com/gosvoh" Icon={SiVk} />
+            <SocialLink href="https://t.me/gosvoh" Icon={SiTelegram} />
+            <SocialLink href="https://github.com/gosvoh" Icon={SiGithub} />
+            <SocialLink
+              href="https://discord.com/users/237653717690744832"
+              Icon={SiDiscord}
+            />
+          </div>
         </section>
         <section>
           <h2>Проекты</h2>
